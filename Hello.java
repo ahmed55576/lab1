f76567c1722c63fc9beb680fc3295271f7c45799
ahmed55576 <59940581+ahmed55576@users.noreply.github.com>
2020-01-15 18:08:13 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+ //Hello.java
+ 
+ //Ahmed Ismail
+ //1/15/2020
+ //001
+ 
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Ahmed Ismail");
+		System.out.println("Lab 1");
+		System.out.println("installing and starting java");
+		System.out.println("1/15/2020");
+		System.out.println("001");
+		System.out.println();
+	}
+}
